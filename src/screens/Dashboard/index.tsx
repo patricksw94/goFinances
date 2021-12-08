c096903de63d7722452396a83d151572818db657
Patrick de Souza Lima <patricksw94@gmail.com>
2021-12-07 22:06:33 -0300

    colors global

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 export function Dashboard(){
   return (
       <Container>
-          <Text>Dashboard</Text>
+          <Title>Dashboard</Title>
       </Container>
     )
 }
