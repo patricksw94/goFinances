bd9762da096a7b5da995ba34b6e77ef60308bfda
Patrick de Souza Lima <patricksw94@gmail.com>
2021-12-09 13:55:47 -0300

    RFPercentage e RFValue

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
 
-import { Container, Header } from './styles';
+import { Container, Header, UserInfo, Photo, User, UserGreeting, UserName, UserWrapper } from './styles';
 
 export function Dashboard(){
   return (
       <Container>
           <Header>
-              <Text>Header</Text>
+            <UserWrapper>
+            <UserInfo>
+              <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/61481164?v=4' }} 
+              />
+              <User>
+                <UserGreeting>Olá,</UserGreeting>
+                <UserName>Patrick</UserName>
+              </User>
+            </UserInfo>
+            </UserWrapper>
           </Header>
 
       </Container>
