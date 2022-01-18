dc0cc8dc60819cb8eeb3596fe7b1b8300cc20761
Patrick de Souza Lima <patricksw94@gmail.com>
2022-01-18 17:01:41 -0300

    button income e outcome estilizados

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
 
-import { Container, Header, Title, Form, Fields } from './styles';
+import { Container, Header, Title, Form, Fields, TransactionsType } from './styles';
 
 export function Register() {
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionsTypeSelect(type: 'up' | 'down'){
+        setTransactionType(type);
+    }
+
     return (
         <Container>
             <Header>
@@ -20,6 +27,21 @@ export function Register() {
                 <Input
                     placeholder="Preço"
                 />
+
+            <TransactionsType>
+                <TransactionTypeButton 
+                    type="up"
+                    title="Income"
+                    onPress={() => handleTransactionsTypeSelect('up')}
+                    isActive={transactionType === 'up'}
+                />
+                <TransactionTypeButton 
+                    type="down"
+                    title="Outcome"
+                    onPress={() => handleTransactionsTypeSelect('down')}
+                    isActive={transactionType === 'down'}
+                />
+            </TransactionsType>
                 </Fields>
 
                 <Button title="Enviar"/>
