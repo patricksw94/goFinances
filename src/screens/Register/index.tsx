b3ba603bc51e9822546a548287f3f1ae51c1e5a2
Patrick de Souza Lima <patricksw94@gmail.com>
2022-01-18 20:15:20 -0300

    Category criado

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { CategorySelect } from '../../components/Form/CategorySelect';
 
 import { Container, Header, Title, Form, Fields, TransactionsType } from './styles';
 
@@ -42,6 +43,8 @@ export function Register() {
                     isActive={transactionType === 'down'}
                 />
             </TransactionsType>
+
+            <CategorySelect title="Categoria" />
                 </Fields>
 
                 <Button title="Enviar"/>
