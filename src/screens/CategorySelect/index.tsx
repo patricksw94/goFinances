5011f54fcae91ee2ef5aeb23a411585dccd6e8e2
Patrick de Souza Lima <patricksw94@gmail.com>
2022-01-25 00:25:55 -0300

    modal criado

--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -12,12 +12,21 @@ interface Category {
 }
 
 interface Props {
-    category: string;
+    category: Category;
     setCategory: (category: Category) => void;
     closeSelectCategory: () => void;
 }
 
-export function CategorySelect({category, setCategory, closeSelectCategory} : Props){
+export function CategorySelect({
+    category,
+    setCategory,
+    closeSelectCategory
+} : Props){
+
+    function handleCategorySelect(category: Category){
+        setCategory(category);
+    }
+
     return(
         <Container>
             <Header>
@@ -29,7 +38,10 @@ export function CategorySelect({category, setCategory, closeSelectCategory} : Pr
             style={{ flex: 1, width: '100%'}}
             keyExtractor={(item) => item.key}
             renderItem={({ item }) => (
-                <Category>
+                <Category
+                    onPress={() => handleCategorySelect(item)}
+                    isActive={category.key === item.key}
+                >
                     <Icon name={item.icon} />
                     <Name>{item.name}</Name>
                 </Category>
@@ -38,7 +50,10 @@ export function CategorySelect({category, setCategory, closeSelectCategory} : Pr
             />
 
             <Footer>
-                <Button title="Selecionar" />
+                <Button
+                 title="Selecionar"
+                 onPress={closeSelectCategory}
+                 />
             </Footer>
         </Container>
     )
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
+import { Modal } from 'react-native';
 
 import { Input } from '../../components/Form/Input';
 import { Button } from '../../components/Form/Button';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
-import { CategorySelect } from '../../components/Form/CategorySelect';
+import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
+
+import { CategorySelect } from '../CategorySelect';
 
 import { Container, Header, Title, Form, Fields, TransactionsType } from './styles';
 
 export function Register() {
     const [transactionType, setTransactionType] = useState('');
+    const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+    const [category, setCategory] = useState({
+        key: 'category',
+        name: 'Categoria'
+    });
 
     function handleTransactionsTypeSelect(type: 'up' | 'down'){
         setTransactionType(type);
+    }
+
+    function handleOpenSelectCategoryModal(){
+        setCategoryModalOpen(true);
+    }
+
+    function handleCloseSelectCategoryModal(){
+        setCategoryModalOpen(false);
     }
 
     return (
@@ -44,11 +61,22 @@ export function Register() {
                 />
             </TransactionsType>
 
-            <CategorySelect title="Categoria" />
+            <CategorySelectButton 
+            title={category.name}
+            onPress={handleOpenSelectCategoryModal}
+            />
                 </Fields>
 
                 <Button title="Enviar"/>
             </Form>
+
+            <Modal visible={categoryModalOpen}>
+                <CategorySelect 
+                    category={category}
+                    setCategory={setCategory}
+                    closeSelectCategory={handleCloseSelectCategoryModal}
+                />
+            </Modal>
 
         </Container>
     );
